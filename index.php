c5da1ec82c4fd2faeb71601b046b477c584ca04c
isto2278 <timpietsch.prv@gmail.com>
2022-11-27 17:05:07 +0100

    quick action labels, entries in loop

--- index.php
+++ index.php
@@ -16,96 +16,11 @@
       </section>
     </section>
     <section class="planned-entries">
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review" selected>Review</option>
-          <option value="et-stream">Stream</option>
-          <option value="et-article">Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1" selected>Average_Snek</option>
-          <option value="2">Bok</option>
-          <option value="3">Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review">Review</option>
-          <option value="et-stream" selected>Stream</option>
-          <option value="et-article">Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1">Average_Snek</option>
-          <option value="2" selected>Bok</option>
-          <option value="3">Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review">Review</option>
-          <option value="et-stream">Stream</option>
-          <option value="et-article" selected>Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1">Average_Snek</option>
-          <option value="2">Bok</option>
-          <option value="3" selected>Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review">Review</option>
-          <option value="et-stream">Stream</option>
-          <option value="et-article" selected>Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1">Average_Snek</option>
-          <option value="2">Bok</option>
-          <option value="3" selected>Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review">Review</option>
-          <option value="et-stream">Stream</option>
-          <option value="et-article" selected>Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1">Average_Snek</option>
-          <option value="2">Bok</option>
-          <option value="3" selected>Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
-      <article class="entry card">
-        <select name="entry-type" id="">
-          <option value="et-review">Review</option>
-          <option value="et-stream">Stream</option>
-          <option value="et-article" selected>Artikel</option>
-        </select>
-        <input type="text" name="game-title" id="">
-        <select name="user" id="">
-          <option value="1">Average_Snek</option>
-          <option value="2">Bok</option>
-          <option value="3" selected>Isto</option>
-          <option value="4">McGlitch</option>
-        </select>
-        <input type="date" name="" id="">
-      </article>
+      <?php
+        for($i=0;$i<10;$i++) {
+          include "php/entry.php";
+        }
+      ?>
     </section>
     <section>
       <ul class="quick-actions">
---
+++ php/entry.php
@@ -0,0 +1,15 @@
+<article class="entry card">
+  <select name="entry-type" id="">
+    <option value="et-review" selected>Review</option>
+    <option value="et-stream">Stream</option>
+    <option value="et-article">Artikel</option>
+  </select>
+  <input type="text" name="game-title" id="">
+  <select name="user" id="">
+    <option value="1" selected>Average_Snek</option>
+    <option value="2">Bok</option>
+    <option value="3">Isto</option>
+    <option value="4">McGlitch</option>
+  </select>
+  <input type="date" name="" id="">
+</article>
