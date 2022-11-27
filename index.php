3de577c88c2a8ca0a7fbdecc16cc03fa72e4c3c5
isto2278 <timpietsch.prv@gmail.com>
2022-11-27 17:39:53 +0100

    moved quick-actions

--- index.php
+++ index.php
@@ -22,13 +22,11 @@
         }
       ?>
     </section>
-    <section>
-      <ul class="quick-actions">
-        <li class="quick-action__button" title=""><label for="">Neues Review</label><button><i class="fa-solid fa-star"></i></button></li>
-        <li class="quick-action__button" title=""><label for="">Neuer Stream</label><button><i class="fa-solid fa-play"></i></button></li>
-        <li class="quick-action__button" title=""><label for="">Neuer Artikel</label><button><i class="fa-solid fa-file-lines"></i></button></li>
-      </ul>
-    </section>
   </main>
+  <section>
+    <?php
+      include "php/quick-actions/quick-actions.php";
+    ?>
+  </section>
 </body>
 </html>
