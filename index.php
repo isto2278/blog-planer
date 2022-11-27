fb8ea8f342462d881705b4cd311569d081f37310
isto2278 <timpietsch.prv@gmail.com>
2022-11-27 13:08:35 +0100

    added basic html and css

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Devoted Nonsense</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <main>
+    <section class="planned-entries">
+      <article class="entry">
+        <select name="entry-type" id="">
+          <option value="et-review">Review</option>
+          <option value="et-stream">Stream</option>
+          <option value="et-article">Artikel</option>
+        </select>
+        <input type="text" name="game-title" id="">
+        <select name="user" id="">
+          <option value="1">Average_Snek</option>
+          <option value="2">Bok</option>
+          <option value="3">Isto</option>
+          <option value="4">McGlitch</option>
+        </select>
+        <input type="date" name="" id="">
+      </article>
+    </section>
+    <section>
+      <ul class="quick-action">
+        <li class="quick-action__button" title=""><label for=""></label><input type="button" value=""></li>
+        <li class="quick-action__button" title=""><label for=""></label><input type="button" value=""></li>
+        <li class="quick-action__button" title=""><label for=""></label><input type="button" value=""></li>
+      </ul>
+    </section>
+  </main>
+</body>
+</html>
