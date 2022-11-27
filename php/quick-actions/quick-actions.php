340b5480d582bb3e46b4edd03c77db990c070d03
isto2278 <timpietsch.prv@gmail.com>
2022-11-27 18:51:43 +0100

    tested different structure

--- php/quick-actions/quick-actions.php
+++
@@ -1,5 +0,0 @@
-<ul class="quick-actions">
-  <li class="quick-action__button" title=""><label for="">Neues Review</label><button><i class="fa-solid fa-star"></i></button></li>
-  <li class="quick-action__button" title=""><label for="">Neuer Stream</label><button><i class="fa-solid fa-play"></i></button></li>
-  <li class="quick-action__button" title=""><label for="">Neuer Artikel</label><button><i class="fa-solid fa-file-lines"></i></button></li>
-</ul>
